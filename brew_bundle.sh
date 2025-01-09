2f75f4c01f3338fb0289ab1b8ab1542060d9edab
Ryan Jinnette <110581203+rjinnette@users.noreply.github.com>
2025-01-08 22:06:35 -0600

    Update 01/08/2025

---
+++ brew_bundle.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+# Source the .zsh_profile to ensure the aliases are available
+source ~/.zprofile
+
+# Run brew bundle dump to save the Brewfile
+brew bundle dump --force
+
+# Check the Git status
+git_status=$(git status --porcelain)
+
+# If there are changes, stage, commit, and push
+if [[ -n "$git_status" ]]; then
+    add  # Use the alias for git add
+    cmt "Update $(date +'%m/%d/%Y')"  # Use the alias for git commit
+    push  # Use the alias for git push
+else
+    echo "No changes to commit."
+    exit 0  # Exit the script cleanly if no changes are found
+fi
+
+
+
